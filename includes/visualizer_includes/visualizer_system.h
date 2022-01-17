1d8278531f98bb352695f04439a9b9e08a3cc738
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-01-17 14:32:34 +0100

    ⚡️Now the window will never be wider than 1920 pixels.

--- includes/visualizer_includes/visualizer_system.h
+++ includes/visualizer_includes/visualizer_system.h
@@ -6,14 +6,14 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 15:12:19 by ldutriez          #+#    #+#             */
-/*   Updated: 2021/03/25 18:02:51 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/01/17 14:22:40 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VISUALIZER_SYSTEM_H
 # define VISUALIZER_SYSTEM_H
 
-# define S_WD 2000
+# define S_WD 1920
 # define S_HT 600
 
 /*
